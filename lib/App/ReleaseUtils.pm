1e9b4cf94ca4d5c94450852429abf8ee681b4563
perlancar (on pc-home) <perlancar@gmail.com>
2017-02-05 14:06:04 +0700

    Initial commit

---
+++ lib/App/ReleaseUtils.pm
@@ -0,0 +1,22 @@
+package App::ReleaseUtils;
+
+# DATE
+# VERSION
+
+use 5.010001;
+use strict;
+use warnings;
+
+our %SPEC;
+
+1;
+# ABSTRACT: Collection of utilities related to software releases
+
+=head1 SYNOPSIS
+
+This distribution provides the following command-line utilities related to
+software releases:
+
+#INSERT_EXECS_LIST
+
+=cut
